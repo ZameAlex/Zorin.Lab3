70a0623b6bdb92d13061ad6757cc3525acaa08c7
Alex <Alex@DESKTOP-3MOEDP1>
2018-12-09 15:49:10 +0200

    Project type changed;  Add operator +,-,/ for vector

---
+++ Matrix/ReverseMatrixNotExistsException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrix
+{
+    class ReverseMatrixNotExistsException:ApplicationException
+    {
+        public ReverseMatrixNotExistsException():base("Matrix has no reverse!")
+        {
+                
+        }
+    }
+}
